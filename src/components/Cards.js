74efaf03bb4cdd1456b7af3f89f18b734df1f45a
Rachael Cole <jcrachael@gmail.com>
2022-11-28 11:51:31 +1100

    Cards display in random order

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,27 @@
 import Card from "./Card";
+import pokemon from "../pokemon";
 import "../styles/Cards.css";
 
 function Cards() {
-  return (
-    <div className="Cards">
-      <Card poke="Bulbasaur" />
-      <Card poke="Squirtle" />
-      <Card poke="Charmander" />
-      <Card poke="Pikachu" />
-      <Card poke="Diglett" />
-      <Card poke="Jigglypuff" />
+  const list = pokemon
+    .map((poke) => {
+      return <Card key={poke.id} poke={poke} handleClick={handleClick} />;
+    })
+    .sort(function () {
+      return Math.random() - 0.5;
+    });
 
-      <Card poke="Geodude" />
-      <Card poke="Pidgey" />
-      <Card poke="Machop" />
-      <Card poke="Drowzee" />
-      <Card poke="Gastly" />
-      <Card poke="Dratini" />
-    </div>
-  );
+  function handleClick(e) {
+    if (e.target.className === "poke-img") {
+      console.log(e.target.parentElement.className + " clicked");
+      e.stopPropagation();
+    } else {
+      console.log(e.target.className + " clicked");
+      e.stopPropagation();
+    }
+  }
+
+  return <div className="Cards">{list}</div>;
 }
 
 export default Cards;
---
+++ src/pokemon.js
@@ -0,0 +1,29 @@
+import bulbasaur from "./assets/pokemon/bulbasaur.png";
+import squirtle from "./assets/pokemon/squirtle.png";
+import charmander from "./assets/pokemon/charmander.png";
+import diglett from "./assets/pokemon/diglett.png";
+import machop from "./assets/pokemon/machop.png";
+import pidgey from "./assets/pokemon/pidgey.png";
+import drowzee from "./assets/pokemon/drowzee.png";
+import dratini from "./assets/pokemon/dratini.png";
+import geodude from "./assets/pokemon/geodude.png";
+import jigglypuff from "./assets/pokemon/jigglypuff.png";
+import gastly from "./assets/pokemon/gastly.png";
+import pikachu from "./assets/pokemon/pikachu.png";
+
+const pokemon = [
+  { id: 0, name: "Bulbasaur", img: bulbasaur },
+  { id: 1, name: "Squirtle", img: squirtle },
+  { id: 2, name: "Charmander", img: charmander },
+  { id: 3, name: "Diglett", img: diglett },
+  { id: 4, name: "Machop", img: machop },
+  { id: 5, name: "Pidgey", img: pidgey },
+  { id: 6, name: "Drowzee", img: drowzee },
+  { id: 7, name: "Dratini", img: dratini },
+  { id: 8, name: "Geodude", img: geodude },
+  { id: 9, name: "Jigglypuff", img: jigglypuff },
+  { id: 10, name: "Gastly", img: gastly },
+  { id: 11, name: "Pikachu", img: pikachu },
+];
+
+export default pokemon;
